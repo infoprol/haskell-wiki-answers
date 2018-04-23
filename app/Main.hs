0b1fd7c5b887252d12ac369ced3297f81a2f24e0
matt jaehn <bitmatt.thelab@gmail.com>
2018-04-22 22:53:10 -0500

    gcd'' written but off-by-one seq error in checking termination condition of euclid's meth.

--- app/Main.hs
+++ app/Main.hs
@@ -12,12 +12,24 @@ testgcd' = do
 
 
 
+testgcd'' :: IO ()
+testgcd'' = do
+  let d = gcd'' 36 28 :: Integer
+  putStrLn $ "gcd'' 36 28 = " ++ (show d) ++ "."
+  let e = gcd'' 77 847 :: Integer
+  putStrLn $ "gcd'' 77 847 = " ++ (show e) ++ "."
+  let dd = gcd'' 28 36 :: Integer
+  putStrLn $ "gcd'' 28 36 = " ++ (show d) ++ "."
+
+
 
 
 main :: IO ()
 main = do
   putStrLn "Test suite implemented shittily."
   testgcd'
+  putStrLn "now for gcd''..."
+  testgcd''
   putStrLn "done-yet."
 
 
--- src/GCDRefactorum.hs
+++ src/GCDRefactorum.hs
@@ -2,10 +2,10 @@ module GCDRefactorum
     ( someFunc
     , gcd'
     , gcd''
-    , gcdTry
     ) where
 
-import Control.Monad
+--import Control.Monad
+--import Data.List
 
 someFunc :: IO ()
 someFunc = putStrLn "suck this forever!!"
@@ -13,19 +13,93 @@ someFunc = putStrLn "suck this forever!!"
 
 {--
   special cases / terminating conditions all flim-flammed together-like.
+  also, doesn't work right.
 --}
-gcd' :: Integer -> Integer -> Integer
-gcd' m 0 = m
-gcd' 0 _ = 0
-gcd' m n
-    | m < 0             = gcd' (abs m) n
-    | n < 0             = gcd' m (abs n)
-    | m < n             = gcd' n m
+{--
+xxgcd' :: Integereger -> Integereger -> Integereger
+xxgcd' m 0 = m
+xxgcd' 0 _ = 0
+xxgcd' m n
+    | m < 0             = xxgcd' (abs m) n
+    | n < 0             = xxgcd' m (abs n)
+    | m < n             = xxgcd' n m
     | m == n            = n
-    | otherwise         = gcd' (m `div` n) (m `mod` n)
+    | otherwise         = xxgcd' (m `div` n) (m `mod` n)
+--}
+
+
+-- might not work right all/most of time either,
+-- at least it's shorter...
+gcd' :: Integer -> Integer -> Integer
+gcd' a b    | a < 0         = gcd' (abs a) b
+            | b < 0         = gcd' a (abs b)
+            | b == 0        = a
+            | a < b         = gcd' b a
+            | otherwise     = gcd' (a `div` b) (a `rem` b)
 
 
 
+
+
+
+{---------------------------------------
+
+    gcd'' - second go, now with lists and shit!
+
+--}
+(//) :: Integer -> Integer -> Integer
+(//) x y = x `div` y
+
+(%%) :: Integer -> Integer -> Integer
+(%%) x y = x `mod` y
+
+ 
+euclidsElems :: Integer -> Integer -> [ (Integer, Integer, Integer) ]
+euclidsElems a b = (b, q, r) : euclidsElems q r
+    where
+        q = a // b
+        r = a %% b
+
+isAnswer :: (Integer, Integer, Integer) -> Bool
+isAnswer (_, _, r)    | r == 0    = True
+                    | otherwise = False
+
+pluckAns :: (Integer, Integer, Integer) -> Integer
+pluckAns eucliment = x where (x, _, _) = eucliment
+
+
+gcd'' :: Integer -> Integer -> Integer
+gcd'' a b = pluckAns $ (filter isAnswer (euclidsElems a b)) !! 0
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+{---
+    where
+        ans _ _ = get$ takeWhile reRemains (euclidsElements a b)
+        reRemains 
+        euclidsElements x y = (y (x `div` y) (x `mod` y)) (:) 
 
 
 data Try a = Err String | Return a
@@ -50,14 +124,17 @@ instance Monad Try where
  
 
 
-gcd'' :: Integer -> Integer -> Integer
+gcd'' :: Integereger -> Integereger -> Integereger
 gcd'' m 0 = m
 gcd'' m n = gcd'' (m `div` n) (m `mod` n)
 
 
-gcdTry :: Integer -> Integer -> Try Integer
+gcdTry :: Integereger -> Integereger -> Try Integereger
 gcdTry m n
     | or [ m <= 0, n <= 0 ]     = Err "positive numbers only, goddammit!"
     | m < n                     = Err "arguments must be fed in non-increasing order, goddammit!"
     | otherwise                 = Return (gcd'' m n)
+    
+    
+--}
 
